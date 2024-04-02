1aec374a6872edf489d0a48d3b92aac867a957e6
msaidbasmaci <muhammedbsmcm@gmail.com>
2024-04-02 17:45:28 +0300

    Initial Commit

---
+++ SwiftUITicTacToe/SwiftUITicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITicTacToeApp.swift
+//  SwiftUITicTacToe
+//
+//  Created by Muhammed Said BASMACI on 2.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
